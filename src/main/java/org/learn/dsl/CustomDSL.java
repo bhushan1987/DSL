f90b83049f58f8e855ea95e079af39b9c966fdde
bhushan1987 <bhushan.karmarkar12@gmail.com>
2021-05-14 11:26:25 +0530

    Initial commit

---
+++ src/main/java/org/learn/dsl/CustomDSL.java
@@ -0,0 +1,10 @@
+package org.learn.dsl;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomDSL {
+    public Object authenticate(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null; // Write your implementation here
+    }
+}
